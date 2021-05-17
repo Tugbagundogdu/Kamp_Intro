62906bebc7fd0c1d606a7f21592cf3de925ca67d
Tuğba Gündoğdu <tugba.gundgdu@gmail.com>
2021-05-17 22:54:17 +0300

    17.05.2021

---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 2001 && gamer.FirstName == "TUĞBA" && gamer.LastName == "GÜNDOĞDU" && gamer.IdentityNumber == 123456)
+            {
+                return true;
+            }
+            else
+            {
+                return false;     
+            }
+        }
+
+    }
+}
